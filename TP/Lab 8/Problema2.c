4ecc12f4b3bdb5c6d88a21d5ffa7a041d5402314
StudentUPT <uptacinfo@gmail.com>
2019-04-18 19:47:45 +0300

    ok

--- TP/Lab 8/Problema2.c
+++ TP/Lab 8/Problema2.c
@@ -1,23 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-void citeste(char **nume,int *varsta){
-  char s[50]; int nr=0;
-  printf("Introduceti numele: ");
-  scanf("%40s",s);
-   printf("Introduceti varsta: "); 
-   scanf("%d",&nr);
-    *varsta=nr;
-}
-void scrie(char *nume,int varsta)
+void citeste(char **nume, int *varsta)
 {
-    printf("%s,%d\n",nume,varsta);
+    *nume = (char *)malloc(4 * sizeof(char));
+    printf("Introduceti numele: ");
+    scanf("%40s", *nume);
+    printf("Introduceti varsta: ");
+    scanf("%d", &(*varsta));
+}
+void scrie(char *nume, int varsta)
+{
+    printf("%s,%d\n", nume, varsta);
 }
 int main(void)
-{   char *nume;
+{
+    char *nume;
     int varsta;
-    citeste(&nume,&varsta);
-    scrie(nume,varsta);
+    citeste(&nume, &varsta);
+    scrie(nume, varsta);
     free(nume);
 
     return 0;
